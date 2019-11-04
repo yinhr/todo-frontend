4f76f8a5aa14309e2d89e1c58c9aa5db9d791473
yinhr <inhr.ytk@gmail.com>
2019-11-05 01:59:33 +0900

    Add link to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,48 @@
 <template>
-  <footer id="footer" class="bg-gray-700 min-w-full">
+  <footer id="footer" class="bg-gray-700">
+    <div class="md:flex md:justify-around max-w-5xl mx-auto px-4 py-3 text-gray-200">
+      <router-link to="/home" class="block p-2">
+        <div class="font-black">Home</div>
+      </router-link>
+      <div class="p-2">
+        <div class="font-black">About</div>
+        <div class="p-2">
+          <a href="https://www.notion.so/prmcy/ToDo-14f83b283c4b4bd088ee9f11ebe5be13" target="_blank">
+            <font-awesome-icon icon="external-link-alt" class="self-center mr-2 text-sm" />
+            <span>TODO管理アプリについて</span>
+          </a>
+        </div>
+      </div>
+      <div class="p-2">
+        <div class="font-black">Github</div>
+        <div class="p-2">
+          <a href="https://github.com/yinhr/todo-terraform" target="_blank">
+            <font-awesome-icon icon="external-link-alt" class="self-center mr-2 text-sm" />
+            <span>yinhr/todo-terraform</span>
+          </a>
+        </div>
+        <div class="p-2">
+          <a href="https://github.com/yinhr/todo-backend" target="_blank">
+            <font-awesome-icon icon="external-link-alt" class="self-center mr-2 text-sm" />
+            <span>yinhr/todo-backend</span>
+          </a>
+        </div>
+        <div class="p-2">
+          <a href="https://github.com/yinhr/todo-frontend" target="_blank">
+            <font-awesome-icon icon="external-link-alt" class="self-center mr-2 text-sm" />
+            <span>yinhr/todo-frontend</span>
+          </a>
+        </div>
+        <div class="p-2">
+          <a href="https://github.com/yinhr/todo-nginx" target="_blank">
+            <font-awesome-icon icon="external-link-alt" class="self-center mr-2 text-sm" />
+            <span>yinhr/todo-nginx</span>
+          </a>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
 <script>
 </script>
-
-<style>
-#footer {
-  height: 200px;
-}
-</style>
